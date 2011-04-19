6b1475981d4f28f44e3cbf2c14a2ed1bde79151f
David Martinez Marti <desarrollo@gestiweb.com>
2011-04-19 12:04:54 +0200

    Add forms to various actions

--- qt4client/mainwindow.py
+++ qt4client/mainwindow.py
@@ -6,6 +6,8 @@ from PyQt4 import QtGui, QtCore, uic
 
 from widgets import llampexmainmenu, llampexgroupbutton
 
+from masterform import LlampexMasterForm
+
 import projectloader
 
 
@@ -43,29 +45,7 @@ class LlampexMdiSubWindow(QtGui.QMdiSubWindow):
                 del self.windowdict[self.windowkey]
         finally:
             event.accept()
-        
-class LlampexMasterForm(QtGui.QWidget):        
-    def __init__(self, windowkey, actionobj):
-        QtGui.QWidget.__init__(self)
-        self.windowkey = windowkey
-        self.actionobj = actionobj
-        try:
-            ui_filepath = self.actionobj.filedir(self.actionobj.master["form"])
-            self.ui = uic.loadUi(ui_filepath,self)
-        except Exception:
-            self.layout = QtGui.QVBoxLayout()
-            self.layout.addStretch()
-            
-            label = QtGui.QLabel("FATAL: An error ocurred trying to load the master form:")
-            self.layout.addWidget(label)
-            text = QtGui.QTextBrowser()
-            text.setText(traceback.format_exc())
-            self.layout.addWidget(text)
-
-            self.layout.addStretch()
-            self.setLayout(self.layout)
-            
-        
+  
 
 class LlampexMainWindow(QtGui.QMainWindow):
     def prjdir(self, x):
