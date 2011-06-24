e9fd74bd7fedec95d534c77e9784e688ac42726b
David Martinez Marti <desarrollo@gestiweb.com>
2011-06-24 12:41:50 +0200

    Bugfixes for speed

--- projects/erp/generic/master/scripts/masterbasic2.py
+++ projects/erp/generic/master/scripts/masterbasic2.py
@@ -18,7 +18,7 @@ class MasterScript(object):
             self.rpc.qtdriver = qtdriver.QSqlLlampexDriver(self.rpc)
             self.rpc.qtdb = QtSql.QSqlDatabase.addDatabase(self.rpc.qtdriver, "llampex-qsqlrpcdriver")
             assert(self.rpc.qtdb.open("",""))
-            #qtdriver.DEBUG_MODE = False
+            qtdriver.DEBUG_MODE = False
         self.db = self.rpc.qtdb
         self.table = self.form.actionobj.table
         self.model = None
@@ -30,22 +30,30 @@ class MasterScript(object):
         
         self.model = QtSql.QSqlTableModel(None,self.db)
         self.modelReady = threading.Event()
-        QtCore.QTimer.singleShot(100,self.settablemodel)
+        self.modelSet = threading.Event()
+        QtCore.QTimer.singleShot(5,self.settablemodel)
         thread1 = threading.Thread(target=self.reload_data)
         thread1.start()
         
         
     def reload_data(self):
         #table = self.form.ui.table
+        print "Model table:", self.table
         self.model.setTable(self.table)
+        print "ok"
         self.model.setEditStrategy(QtSql.QSqlTableModel.OnManualSubmit)
         self.modelReady.set()
+        self.modelSet.wait()
         # QtCore.QTimer.singleShot(100,self.settablemodel)
+        print "Selecting!"
         self.model.select()
+        print "ok"
     
     def settablemodel(self):
         self.modelReady.wait()
-        print "Setting Table Model!"
+        print ">Setting Table Model!"
         self.form.ui.table.setModel(self.model)
+        print ">ok"
+        self.modelSet.set()
             
         
