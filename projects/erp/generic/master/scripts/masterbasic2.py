5b49c8847a275ada0c74096595267cecc37ff464
David Martinez Marti <desarrollo@gestiweb.com>
2011-06-27 10:18:30 +0200

    Add filter options and Context menu on table

--- projects/erp/generic/master/scripts/masterbasic2.py
+++ projects/erp/generic/master/scripts/masterbasic2.py
@@ -1,5 +1,4 @@
 # encoding: UTF-8
-print "loading customers.py"
 import os.path, traceback
 from PyQt4 import QtGui, QtCore, uic
 from PyQt4 import QtSql
@@ -8,6 +7,9 @@ import time
 import re
 import qsqlrpcdriver.qtdriver as qtdriver
 import threading
+
+def h(*args): return os.path.realpath(os.path.join(os.path.dirname(os.path.abspath( __file__ )), *args))
+    
 
 class MasterScript(object):
     def __init__(self, form):
@@ -18,7 +20,7 @@ class MasterScript(object):
             self.rpc.qtdriver = qtdriver.QSqlLlampexDriver(self.rpc)
             self.rpc.qtdb = QtSql.QSqlDatabase.addDatabase(self.rpc.qtdriver, "llampex-qsqlrpcdriver")
             assert(self.rpc.qtdb.open("",""))
-            #qtdriver.DEBUG_MODE = False
+            qtdriver.DEBUG_MODE = False
         self.db = self.rpc.qtdb
         self.table = self.form.actionobj.table
         self.model = None
@@ -30,6 +32,22 @@ class MasterScript(object):
         
         tableheader = table.horizontalHeader()
         tableheader.setSortIndicator(0,0)
+        tableheader.setContextMenuPolicy( QtCore.Qt.ActionsContextMenu )
+        action_addfilter = QtGui.QAction(
+                    QtGui.QIcon(h("../../icons/page-zoom.png")),
+                    "Add &Filter...", tableheader)
+        action_showcolumns = QtGui.QAction(
+                    QtGui.QIcon(h("../../icons/preferences-actions.png")), 
+                    "Show/Hide &Columns...", tableheader)
+        action_hidecolumn = QtGui.QAction("&Hide this Column", tableheader)
+        action_addfilter.setIconVisibleInMenu(True)
+        action_showcolumns.setIconVisibleInMenu(True)
+        tableheader.addAction(action_addfilter)
+        tableheader.addAction(action_showcolumns)
+        tableheader.addAction(action_hidecolumn)
+        tableheader.setStretchLastSection(True)
+         
+        self.form.connect(action_addfilter, QtCore.SIGNAL("triggered(bool)"), self.action_addfilter_triggered)
         
         self.form.connect(tableheader, QtCore.SIGNAL("sortIndicatorChanged(int,Qt::SortOrder)"), self.table_sortIndicatorChanged)
         self.form.connect(tableheader, QtCore.SIGNAL("customContextMenuRequested(QPoint &)"),self.table_headerCustomContextMenuRequested)
@@ -39,6 +57,14 @@ class MasterScript(object):
         self.modelSet = threading.Event()
         QtCore.QTimer.singleShot(5,self.settablemodel)
         thread1 = threading.Thread(target=self.reload_data)
+        thread1.start()
+    
+    def action_addfilter_triggered(self, checked):
+        print "Add Filter triggered:", checked
+        rettext, ok = QtGui.QInputDialog.getText(self.form, "Add New Filter",
+            "Write New WHERE expression:", QtGui.QLineEdit.Normal, self.model.filter())
+        self.model.setFilter(rettext)
+        thread1 = threading.Thread(target=self.select_data)
         thread1.start()
     
     def table_headerCustomContextMenuRequested(self, point):
@@ -57,6 +83,9 @@ class MasterScript(object):
         self.modelReady.set()
         self.modelSet.wait()
         # QtCore.QTimer.singleShot(100,self.settablemodel)
+        self.select_data()
+        
+    def select_data(self):
         print "Selecting!"
         self.model.select()
         print "ok"
